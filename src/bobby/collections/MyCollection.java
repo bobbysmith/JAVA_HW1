cc7868fc55cc82332738b54446546e153a327721
Bobby Smith <bobbysmith24@gmail.com>
2015-10-12 22:54:02 -0500

    failing collections

--- src/bobby/collections/MyCollection.java
+++ src/bobby/collections/MyCollection.java
@@ -26,3 +26,19 @@ public class MyCollection {
 	}
 
 }
+
+
+
+//Create a collection as an instance of ArrayList class. Tokenize the following string by delimiter “  , .”  (blank space, comma and a dot): 
+//
+//In solving a problem of this sort, the grand thing is to be able to reason backwards. That is a very useful accomplishment, and a very easy one, but people do not practice it much. In the every-day affairs of life it is more useful to reason forwards, and so the other comes to be neglected. There are fifty who can reason synthetically for one who can reason analytically.
+//
+//(The above quote is from Sir Arthur Conan Doyle - Sherlock Holmes, in "A Study in Scarlet")
+//
+//Create the list with the tokens populated from the above string. Verify that the collection is not empty. Find the size of the collection. 
+//
+//Iterate over this unsorted list. Do reverse iteration over this unsorted list. Make the list iteration in natural sorting order. Make the list iteration in case insensitive sorting order. 
+//
+//Remove couple of tokens from the collection and iterate again. Clear the collection. Check if it is empty and its size.
+//
+//Print output from all the above operations to the standard output.
